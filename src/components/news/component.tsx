83b90e625889567308cedb77f04647889a7f66a5
DND <Jeff@dnddeveloper.com>
2023-03-25 16:47:08 -0400

    update component

--- src/components/news/component.tsx
+++ src/components/news/component.tsx
@@ -15,25 +15,18 @@ async function readBBCArticle() {
         },
         func: () => {
             let email = document.querySelector("#main-content > article");
-
-            console.log(email);
-
             let lines = email?.childNodes;
-            // let lines = email?.querySelectorAll("div");
-
-
             let emailString = "";
-
-            // emailString += document.querySelector("#\\:24 > div.adn.ads > div.gs > div.gE.iv.gt > table > tbody > tr:nth-child(1) > td.gF.gK > table > tbody > tr > td > h3 > span > span > span")?.textContent;
             lines?.forEach((element) => {
                 if (element.textContent) {
                     let bodyText = (element as HTMLElement).querySelector("#\\:26 > div:nth-child(1)");
                     if (bodyText) {
                         bodyText?.childNodes.forEach((bodyElement) => {
                             if (bodyElement) {
+                                if (!bodyElement.textContent?.toString().includes("<img")) {
 
-                                emailString += bodyElement.textContent + " ";
-
+                                    emailString += bodyElement.textContent + " ";
+                                }
                             } else {
                                 emailString += element.textContent;
                             }
@@ -42,12 +35,9 @@ async function readBBCArticle() {
                         emailString += element.textContent + " ";
                     }
                 } else {
-                    console.log("no content");
                     emailString += " ";
                 }
             })
-
-            console.log(emailString);
             return emailString
         }
     })
@@ -66,26 +56,26 @@ async function readCNNArticle() {
             tabId: currentTabId
         },
         func: () => {
-            let email = document.querySelector("body > div.layout__content-wrapper.layout-with-rail__content-wrapper > section.layout__wrapper.layout-with-rail__wrapper > section.layout__main-wrapper.layout-with-rail__main-wrapper > section.layout__main.layout-with-rail__main > article > section > main > div.article__content-container");            
+            let email = document.querySelector("body > div.layout__content-wrapper.layout-with-rail__content-wrapper > section.layout__wrapper.layout-with-rail__wrapper > section.layout__main-wrapper.layout-with-rail__main-wrapper > section.layout__main.layout-with-rail__main > article > section > main > div.article__content-container");
             console.log(email?.textContent);
 
-        
+
             return email?.textContent;
         }
     })
     return data[0].result;
 }
 
-function cleanBBCData(data:string){
+function cleanBBCData(data: string) {
     var tempData = data.substring(0, data.lastIndexOf("Related Topics"));
     return tempData;
 
 }
 
-function cleanCNNData(data:string){
+function cleanCNNData(data: string) {
     var tempData = data.substring(0, data.lastIndexOf("Enter your email")) + data.substring(data.lastIndexOf("}"));
     return tempData;
-} 
+}
 
 export function NewsWebsites(props: {
     url: string
@@ -95,34 +85,34 @@ export function NewsWebsites(props: {
     const getbbc = async () => {
         const data = await readBBCArticle();
         const summary = await browser.runtime.sendMessage({
-            mode: "summarize",
+            mode: "news",
             input: cleanBBCData(data)
         });
         console.log(summary)
         setSummary(summary)
     }
 
-    const getcnn = async () =>{
+    const getcnn = async () => {
         const data = await readCNNArticle();
         const summary = await browser.runtime.sendMessage({
-            mode: "summarize",
+            mode: "news",
             input: cleanCNNData(data)
         });
         setSummary(summary);
     }
 
-    async function checkUrl(){
+    async function checkUrl() {
         const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-        
+
         var url = tabs[0].url?.split("/")[2];
 
         console.log(url);
 
-        if(url === 'www.bbc.com'){
+        if (url === 'www.bbc.com') {
             getbbc();
         }
 
-        if(url === 'www.cnn.com'){
+        if (url === 'www.cnn.com') {
             getcnn();
         }
 
