edb61d9c6deb2b6accf52c308471e53221525ee0
Aditya Dora <adityadora277@gmail.com>
2023-03-25 16:29:42 -0400

    Complete CNN and BBC data

--- src/components/news/component.tsx
+++ src/components/news/component.tsx
@@ -55,6 +55,38 @@ async function readBBCArticle() {
 }
 
 
+async function readCNNArticle() {
+
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    const currentTab = tabs[0];
+    const currentTabId = currentTab.id as number;
+
+    const data = await browser.scripting.executeScript({
+        target: {
+            tabId: currentTabId
+        },
+        func: () => {
+            let email = document.querySelector("body > div.layout__content-wrapper.layout-with-rail__content-wrapper > section.layout__wrapper.layout-with-rail__wrapper > section.layout__main-wrapper.layout-with-rail__main-wrapper > section.layout__main.layout-with-rail__main > article > section > main > div.article__content-container");            
+            console.log(email?.textContent);
+
+        
+            return email?.textContent;
+        }
+    })
+    return data[0].result;
+}
+
+function cleanBBCData(data:string){
+    var tempData = data.substring(0, data.lastIndexOf("Related Topics"));
+    return tempData;
+
+}
+
+function cleanCNNData(data:string){
+    var tempData = data.substring(0, data.lastIndexOf("Enter your email")) + data.substring(data.lastIndexOf("}"));
+    return tempData;
+} 
+
 export function NewsWebsites(props: {
     url: string
 }) {
@@ -64,13 +96,41 @@ export function NewsWebsites(props: {
         const data = await readBBCArticle();
         const summary = await browser.runtime.sendMessage({
             mode: "summarize",
-            input: data
+            input: cleanBBCData(data)
         });
+        console.log(summary)
         setSummary(summary)
+    }
+
+    const getcnn = async () =>{
+        const data = await readCNNArticle();
+        const summary = await browser.runtime.sendMessage({
+            mode: "summarize",
+            input: cleanCNNData(data)
+        });
+        setSummary(summary);
+    }
+
+    async function checkUrl(){
+        const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+        
+        var url = tabs[0].url?.split("/")[2];
+
+        console.log(url);
+
+        if(url === 'www.bbc.com'){
+            getbbc();
+        }
+
+        if(url === 'www.cnn.com'){
+            getcnn();
+        }
+
+
     }
     return (
         <div className="wrapper">
-            <button onClick={() => getbbc()}>Summarize Article</button>
+            <button onClick={() => checkUrl()}>Summarize Article</button>
             <p>{summary}</p>
         </div>
 
