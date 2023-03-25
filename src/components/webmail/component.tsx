8d3dad118d98502cf4d655a827224ce5b3a61707
DND <Jeff@dnddeveloper.com>
2023-03-25 16:07:05 -0400

    dlkfasdjf;flkjadsf

--- src/components/webmail/component.tsx
+++ src/components/webmail/component.tsx
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import browser, { Tabs } from "webextension-polyfill";
 import css from "./styles.module.css";
 
 // // // //
 
+async function readEmail() {
+
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    const currentTab = tabs[0];
+    const currentTabId = currentTab.id as number;
+
+    const data = await browser.scripting.executeScript({
+        target: {
+            tabId: currentTabId
+        },
+        func: () => {
+            let email = document.querySelector(".adn.ads");
+            let lines = email?.childNodes;
+            // let lines = email?.querySelectorAll("div");
+            let emailString = "";
+
+            emailString += document.querySelector("#\\:24 > div.adn.ads > div.gs > div.gE.iv.gt > table > tbody > tr:nth-child(1) > td.gF.gK > table > tbody > tr > td > h3 > span > span > span")?.textContent;
+            lines?.forEach((element) => {
+                if (element.textContent) {
+                    let bodyText = (element as HTMLElement).querySelector("#\\:26 > div:nth-child(1)");
+                    if (bodyText) {
+                        bodyText?.childNodes.forEach((bodyElement) => {
+                            if (bodyElement) {
+
+                                emailString += bodyElement.textContent + " ";
+
+                            } else {
+                                emailString += element.textContent;
+                            }
+                        })
+                    } else {
+                        emailString += element.textContent + " ";
+                    }
+                    // element.textContent = "painting";
+                } else {
+                    console.log("no content");
+                    emailString += " ";
+                }
+            })
+            console.log(emailString)
+            return emailString;
+        }
+    })
+    return data[0].result;
+}
 export function WebmailHelper(props: {
     onClickReadEmail: () => void;
-    setFormality: (desired: string) =>  void;
+    setFormality: (desired: string) => void;
 }) {
+
+    const [msg, setmsg] = useState<string>("");
+
+    const doReadEmail = async (formatlity: string) => {
+        setmsg("Reading Email...");
+        const data = await readEmail();
+        setmsg("Generating Email...");
+        const airesp = await browser.runtime.sendMessage({
+            mode: "email",
+            input: data,
+            formality: formatlity
+        })
+        const msg = airesp.choices[0].message.content
+        setmsg(msg);
+    }
     return (
         <div className="row">
             <div className="grid gap-3 grid-cols-2 mt-3 w-full">
                 <button
                     className={css.btn}
                     data-testid="scroll-to-top"
-                    onClick={() => props.setFormality("formal")}
+                    onClick={() => doReadEmail("formal")}
                 >
                     Formal
                 </button>
                 <button
                     className={css.btn}
                     data-testid="scroll-to-bottom"
-                    onClick={() => props.setFormality("informal")}
+                    onClick={() => doReadEmail("informal")}
                 >
                     Informal
                 </button>
@@ -29,17 +90,21 @@ export function WebmailHelper(props: {
                 <button
                     className={css.btn}
                     data-testid="scroll-to-top"
-                    onClick={() => props.setFormality("concise")}
+                    onClick={() => doReadEmail("concise")}
                 >
                     Concise
                 </button>
                 <button
                     className={css.btn}
                     data-testid="scroll-to-bottom"
-                    onClick={() => props.setFormality("funny")}
+                    onClick={() => doReadEmail("funny")}
                 >
                     Funny
                 </button>
+            </div>
+
+            <div className="output">
+                {msg}
             </div>
         </div>
     );
